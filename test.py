a1ab4cb1bcc20786955e36f01cf99f1e8b21aa8a
jstzwj <1103870790@qq.com>
2022-11-15 02:32:13 +0800

    update test

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
 import torch, torchaudio
 
-from hifigan.mel_processing import mel_spectrogram_torch
+from hifigan.mel_processing import mel_spectrogram_torch, spectrogram_torch_audio
 
 # Load checkpoint
-hifigan = torch.hub.load("vtuber-plan/hifi-gan:main", "hifigan_48k", force_reload=True).cuda()
+hifigan = torch.hub.load("vtuber-plan/hifi-gan:main", "hifigan_48k", force_reload=False).cuda()
 
 # Load audio
 wav, sr = torchaudio.load("test.wav")
 assert sr == 48000
-wav = wav.unsqueeze(0).cuda()
 
 mel = mel_spectrogram_torch(wav, 2048, 256, 48000, 512, 2048, 0, None, False)
 
-units = hifigan(mel)
+mel = mel.cuda()
+out = hifigan(mel)
+
+wav_out = out.squeeze(0).cpu()
+
+torchaudio.save("test_out.wav", wav_out, sr)
