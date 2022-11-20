ae533cb776e94554fdacdc2154898c68935361a4
jstzwj <1103870790@qq.com>
2022-11-20 22:16:11 +0800

    update mel bins

--- split.py
+++ split.py
@@ -13,16 +13,20 @@ if __name__ == "__main__":
     with open(args.input, "r", encoding="utf-8") as f:
         lines = f.readlines()
 
+    lines = sorted(lines)
     random.shuffle(lines)
 
     origin_filename = os.path.basename(args.input)
     data_len = len(lines)
 
+    valid_num = int(data_len * 0.001)
+    test_num = int(data_len * 0.001)
+
     with open(os.path.join(args.output, origin_filename.replace(".txt", "_train.txt")), "w", encoding="utf-8") as f:
-        f.writelines(lines[:-20])
+        f.writelines(lines[:-valid_num-test_num])
     
     with open(os.path.join(args.output, origin_filename.replace(".txt", "_valid.txt")), "w", encoding="utf-8") as f:
-        f.writelines(lines[-20:-10])
+        f.writelines(lines[-valid_num-test_num:-test_num])
     
     with open(os.path.join(args.output, origin_filename.replace(".txt", "_test.txt")), "w", encoding="utf-8") as f:
-        f.writelines(lines[-10:])
+        f.writelines(lines[-test_num:])
